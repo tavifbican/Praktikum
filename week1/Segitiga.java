102ccc354f06460d88cf602c610fa281dcaaf846
tavifbican <101091919+tavifbican@users.noreply.github.com>
2022-03-07 16:44:07 +0700

    Create Segitiga.java

---
+++ week1/Segitiga.java
@@ -0,0 +1,15 @@
+package week1;
+
+public class Segitiga {
+	
+	static float Luas(float alas, float tinggi) {
+		return alas * tinggi / 2;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Luas Segitiga dengan alas 6cm dan tinggi 8cm adalah " + Luas(6,8) + "cm");
+	}
+
+}
