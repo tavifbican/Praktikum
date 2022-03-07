eb7f2f7abc1dd8467c9f8c93d97d8a1421019094
tavifbican <101091919+tavifbican@users.noreply.github.com>
2022-03-07 16:43:33 +0700

    Create Lingkaran.java

---
+++ week1/Lingkaran.java
@@ -0,0 +1,14 @@
+package week1;
+
+public class Lingkaran {
+	
+	static float Luas(float diameter) {
+		return (float) (diameter * 3.14);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Keliling lingkaran dengan diameter 10cm adalah " + Luas(10) + "cm");
+	}
+
+}
